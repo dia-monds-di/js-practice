e0bfe66094db3549b0ad0cdf77e7fcb3432204f0
dia-monds-di <diana8899877@gmail.com>
2022-12-30 22:35:14 +0200

    format changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 const numberOfFilms = +prompt("How many films have you already watched?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const lastFilm = prompt("Enter the title of one of the last movies you watched?", ""),
-    filmRating = +prompt("How do you rate the film?", ""),
-    lastFilmSecondTime = prompt("Enter the title of one of the last movies you watched?", ""),
-    filmRatingSecondTime = +prompt("How do you rate the film?", "");
+const lastFilm = prompt(
+    "Enter the title of one of the last movies you watched?",
+    ""
+  ),
+  filmRating = +prompt("How do you rate the film?", ""),
+  lastFilmSecondTime = prompt(
+    "Enter the title of one of the last movies you watched?",
+    ""
+  ),
+  filmRatingSecondTime = +prompt("How do you rate the film?", "");
 
 personalMovieDB.movies[lastFilm] = filmRating;
 personalMovieDB.movies[lastFilmSecondTime] = filmRatingSecondTime;
