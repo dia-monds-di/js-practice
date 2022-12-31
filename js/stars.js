0883002a8bc17f64cd5524a6944f0e0655db6b86
dia-monds-di <diana8899877@gmail.com>
2022-12-31 14:55:28 +0200

    prettier

--- js/stars.js
+++ js/stars.js
@@ -26,9 +26,9 @@ first: for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
     console.log(`Second level: ${j}`);
     for (let k = 0; k < 3; k++) {
-        if (k === 2) break first;
-        // if (k === 2) continue first;
+      if (k === 2) break first;
+      // if (k === 2) continue first;
       console.log(`Third level: ${k}`);
-    } 
+    }
   }
 }
