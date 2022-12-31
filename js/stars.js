58e26e563e6e8397e80c5225a961faf2dd513404
dia-monds-di <diana8899877@gmail.com>
2022-12-31 14:54:29 +0200

    add mark in nested loop

--- js/stars.js
+++ js/stars.js
@@ -3,18 +3,32 @@
 // accidental solution
 let temp = " ";
 for (let i = 1; i < 7; i++) {
-    temp = temp + "*";
-    console.log(temp);
+  temp = temp + "*";
+  console.log(temp);
 }
 
 // right solution
-let result = '';
+let result = "";
 const length = 7;
 
 for (let i = 1; i < length; i++) {
   for (let j = 0; j < i; j++) {
     result += "*";
   }
-  result += '\n';
+  result += "\n";
 }
 console.log(result);
+
+// with mark solution
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First level: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second level: ${j}`);
+    for (let k = 0; k < 3; k++) {
+        if (k === 2) break first;
+        // if (k === 2) continue first;
+      console.log(`Third level: ${k}`);
+    } 
+  }
+}
