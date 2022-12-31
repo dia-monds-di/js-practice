13a5824f7c48f830e9bf702c801e63db7eda00c7
dia-monds-di <diana8899877@gmail.com>
2022-12-31 15:42:29 +0200

    Exercise with loops and conditions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const numberOfFilms = +prompt("How many films have you already watched?", "");
+let lastFilm;
+let filmRating;
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,18 +12,36 @@ const personalMovieDB = {
   privat: false,
 };
 
-const lastFilm = prompt(
+for (let i = 0; i < 2; i++) {
+  lastFilm = prompt(
     "Enter the title of one of the last movies you watched?",
     ""
-  ),
-  filmRating = +prompt("How do you rate the film?", ""),
-  lastFilmSecondTime = prompt(
-    "Enter the title of one of the last movies you watched?",
-    ""
-  ),
-  filmRatingSecondTime = +prompt("How do you rate the film?", "");
+  );
+  while (!lastFilm || lastFilm.length > 50) {
+    lastFilm = prompt(
+      "AGAIN Enter the title of one of the last movies you watched?",
+      ""
+    );
+  }
+  filmRating = +prompt("How do you rate the film?", "");
+  personalMovieDB.movies[lastFilm] = filmRating;
 
-personalMovieDB.movies[lastFilm] = filmRating;
-personalMovieDB.movies[lastFilmSecondTime] = filmRatingSecondTime;
+  //  second option (instead of while)
+  // if (lastFilm != null && filmRating != null && lastFilm != '' && filmRating != '' && lastFilm.length < 50) {
+  //   personalMovieDB.movies[lastFilm] = filmRating;
+  // } else {
+  //   i--;
+  // }
+}
+
+if (personalMovieDB.count < 10) {
+  alert("Watched quite a few films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("You are a classic spectator");
+} else if (personalMovieDB.count > 30) {
+  alert("You are a cinephile");
+} else {
+  alert("Error");
+}
 
 console.log(personalMovieDB);
